9a312816561dae514dfca9cad526178a5b47053c
yeduk3 <yeduk33@gmail.com>
2025-03-17 17:05:06 +0900

    Initial Commit

---
+++ ObjectiveOpenGL/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ObjectiveOpenGL
+//
+//  Created by 이용규 on 3/17/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
